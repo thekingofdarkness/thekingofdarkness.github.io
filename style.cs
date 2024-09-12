4e6d1d2a254a34e88bd3abea2ab41e0e93f66d99
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2024-09-12 23:24:33 +0100

    aut

---
+++ style.cs
@@ -0,0 +1,21 @@
+body {
+    font-family: 'Arial', sans-serif;
+}
+
+header {
+    background-color: #007bff;
+    color: white;
+}
+
+section {
+    padding: 20px;
+    background-color: #f8f9fa;
+    border-radius: 5px;
+    margin-bottom: 20px;
+}
+
+footer {
+    background-color: #343a40;
+    color: white;
+}
+
